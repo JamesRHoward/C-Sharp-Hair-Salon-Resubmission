4ff46d747ea72e01d5dfabb5add22de10275f8b4
James <jrh682@gmail.com>
2016-07-27 20:09:53 -0700

    created GetAll Method for stylist.cs

--- Objects/Stylist.cs
+++ Objects/Stylist.cs
@@ -39,5 +39,35 @@ namespace HairSalon
         return (stylistEquality);
       }
     }
+    public static List<Stylist> GetAll()
+    {
+      List<Stylist> allStylists = new List<Stylist>{};
+
+      SqlConnection conn = DB.Connection();
+      SqlDataReader rdr = null;
+      conn.Open();
+
+      SqlCommand cmd = new SqlCommand("SELECT * FROM stylists;", conn);
+      rdr = cmd.ExecuteReader();
+
+      while(rdr.Read())
+      {
+        int stylistId = rdr.GetInt32(0);
+        string stylistName = rdr.GetString(1);
+        Stylist newStylist = new Stylist(stylistName, stylistId);
+        allStylists.Add(newStylist);
+      }
+
+      if (rdr != null)
+      {
+        rdr.Close();
+      }
+      if (conn != null)
+      {
+        conn.Close();
+      }
+
+      return allStylists;
+    }
   }
 }
