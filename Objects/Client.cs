42de91c12971504dcd7c5f39ed08e36caeb5b555
James <jrh682@gmail.com>
2016-07-27 19:09:18 -0700

    created public boolean override for client.cs

--- Objects/Client.cs
+++ Objects/Client.cs
@@ -4,3 +4,52 @@ using System.Data;
 using System.Data.SqlClient;
 
 namespace HairSalon
+{
+  public class Client
+  {
+    private int _id;
+    private string _clientName;
+    private int _stylistId;
+
+    public Client(stringm clientName, int stylistId, int id = 0)
+    {
+      _id = id;
+      _clientName = clientName;
+      _stylistId = stylistId;
+    }
+    public int GetId()
+    {
+      return _id;
+    }
+    public string GetClientName()
+    {
+      return _clientName;
+    }
+    public int GetStylistId()
+    {
+      return _stylistId;
+    }
+    public void SetClientName(string clientName)
+    {
+      _clientName = clientName;
+    }
+    public void SetSytlistId(int stylistId)
+    {
+      _stylistId = stylistId;
+    }
+    public override bool Equals(System.Object otherClient)
+    {
+      if (!(otherClient is Client))
+      {
+        return false;
+      }
+      else
+      {
+        Client newClient = (Client) otherClient;
+        bool idEquality = this.GetId() == newClient.GetId();
+        bool clientNameEquality = this.GetName() == newClient.GetClientName();
+        return (idEquality && clientNameEquality);
+      }
+    }
+  }
+}
